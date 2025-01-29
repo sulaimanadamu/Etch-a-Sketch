2f8ad415d2b57e35b2e08cc6d3a2540013a4fd66
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-01-29 13:06:20 +0100

    User interaction area, added

--- script.js
+++ script.js
@@ -1,38 +1,101 @@
+'use strict';
 //logic for the app is 
 //create row with 16 divs, then add each row to the container div 16 times
 
 const fillerDiv = document.createElement('div')
 const rowDiv = document.createElement('div')
-const entireContainerDiv = document.createElement('div')
+const tileContainer = document.createElement('div')
+const interactionContainer = document.createElement('div')
+const formFieldCont = document.createElement('div')
+const btnContainer = document.createElement('div')
+const header = document.createElement('header')
 const body = document.querySelector('body')
+const rows = 16
+const cols = 16
 
-entireContainerDiv.style.width = '100vw'
-entireContainerDiv.style.height = '100vh'
-console.log(entireContainerDiv.offsetHeight)
+formFieldCont.id = 'form'
+interactionContainer.id = 'aside'
 
-entireContainerDiv.style.border = '2px solid green'
+header.textContent = 'Etch-a-Sketch'
+console.log(tileContainer.offsetWidth)
 
-
+// tileContainer.style.border = '2px solid green'
+tileContainer.setAttribute('id',"tile-container")
+interactionContainer.setAttribute('id', "interaction-Container")
 fillerDiv.style.width = '100px'
 rowDiv.style.display = "flex"
 rowDiv.style.height = '30px'
 
-
-const rows = 16
-const cols = 16
-
+function buildTile(){
 for (let i = 0; i < cols; i++){
+    fillerDiv.className = 'block'
     rowDiv.appendChild(fillerDiv.cloneNode(true))
 }
-
 for (let i = 0; i < rows; i++){
     const rowClone = rowDiv.cloneNode(true)
-    entireContainerDiv.appendChild(rowClone)
+    rowClone.className = `row${i}`
+    tileContainer.appendChild(rowClone)
+}
 }
 
-body.appendChild(entireContainerDiv)
-entireContainerDiv.addEventListener('click', (e)=>{
-    if(e.target){
-       
+
+tileContainer.addEventListener('mouseover', (e)=>{
+    if(e.target.classList.contains('block')){
+    e.target.style.backgroundColor = 'red'
     }
 })
+
+
+
+function constructForm(formContainer){
+    const rowInput = document.createElement('input')
+    const rowLabel = document.createElement('label')
+    const colInput = document.createElement('input')
+    const colLabel = document.createElement('label')
+    const rowInputBlock = document.createElement('div')
+    const colInputBlock = document.createElement('div')
+    rowLabel.className = "label"
+    colLabel.className = "label"
+    rowInput.placeholder = 'Maximum of 100'
+    colInput.placeholder = 'Maximum of 100'
+    rowInputBlock.id = 'row-block'
+    colInputBlock.id = 'col-block'
+    rowLabel.textContent = "Box per row"
+    colLabel.textContent = "Box per column"
+    rowInput.setAttribute('id', "row-input")
+    rowLabel.setAttribute('for', 'row-input')
+    colInput.setAttribute('id', "col-input")
+    colLabel.setAttribute('for', 'col-input')
+    rowInputBlock.append(rowLabel, rowInput)
+    colInputBlock.append(colLabel, colInput)
+    formContainer.append(rowInputBlock, colInputBlock)
+}
+
+function constructBtns(btnsContainer){
+    const resetBtn = document.createElement('button')
+    const clearBtn = document.createElement('button')
+    resetBtn.textContent = "RESET"
+    clearBtn.textContent = "CLEAR"
+    btnContainer.setAttribute("id", 'btns')
+
+    resetBtn.setAttribute('id', "reset")
+    clearBtn.setAttribute('id', "clear")
+    resetBtn.setAttribute('class', "btn")
+    clearBtn.setAttribute('class', "btn")
+    btnsContainer.append(clearBtn, resetBtn)
+}
+
+function buildAsideBlock(){
+    constructForm(formFieldCont)
+    constructBtns(btnContainer)
+    interactionContainer.append(header, formFieldCont, btnContainer)
+}
+
+function buildDom(parent, ...children){
+    for(let child of children){
+        parent.append(child)
+    }
+}
+buildTile()
+buildAsideBlock()
+buildDom(body, interactionContainer, tileContainer)
